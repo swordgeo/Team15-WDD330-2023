c8c4db8b0616d92c54e49db2f2d8416668aa0cda
swordgeo <swordgeo1094@yahoo.com>
2023-02-25 14:51:07 -0500

    add item working

--- src/js/product.js
+++ src/js/product.js
@@ -1,10 +1,22 @@
-import { setLocalStorage } from "./utils.mjs";
+// import { setLocalStorage } from "./utils.mjs";
+import { getLocalStorage, setLocalStorage } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
 
 const dataSource = new ProductData("tents");
 
+// function addProductToCart(product) {
+//   setLocalStorage("so-cart", product);
+// }
 function addProductToCart(product) {
-  setLocalStorage("so-cart", product);
+  //first I need to grab the cart as it is
+  let cartContents = getLocalStorage("so-cart");
+  //If the cart is empty then .push errors because cartContents isn't an array
+  //Therefore we're gonna create an empty array before moving on
+  if (!Array.isArray(cartContents)) {
+    cartContents = [];
+  }
+  cartContents.push(product);
+  setLocalStorage("so-cart", cartContents);
 }
 // add to cart button event handler
 async function addToCartHandler(e) {
