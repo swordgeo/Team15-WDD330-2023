8b25a2680835f109f88fa469129688a4fea81905
Jermery Wheeler <you@example.com>
2023-03-08 21:00:00 -0500

    updating search with header

--- src/js/search.js
+++ src/js/search.js
@@ -1,24 +1,40 @@
-
-
-// function mySearchFunction() {
+export function mySearchFunction() {
     
-//      //console.log("I was clicked.")//This is for testing purposes. 
+    //console.log("I was clicked.")//This is for testing purposes. 
+    
 
 
-//         let input, filter, ul, li, a, i;
-//         input = document.getElementById("searchBar");
-//         filter = input.value.toUpperCase();
-//         ul = document.getElementById("mySearch");
-//         li = ul.getElementsByTagName("li");
-//         for (i = 0; i< li.length; i++) {
-//             a = li[i].getElementsByTagName("a")[0];
-//             if (a.innerHTML.toUpperCase().indexOf(filter) > - 1) {
-//                 li[i].style.display = "";
-//             } else {
-//                 li[i].style.display = "none";
-//             }
+       let input, filter, ul, li, a, i;
+       input = document.getElementById("searchBar");
+       filter = input.value.toUpperCase();
+       ul = document.getElementById("mySearch");
+       input.addEventListener("change", () =>{
+           alert("I was clicked...")
+           const serchParm = input.value;
+           console.log(serchParm)
+           li = ul.getElementsByTagName("li");
+       for (i = 0; i< li.length; i++) {
+           a = li[i].getElementsByTagName("a")[0];
+           if (a.innerHTML.toUpperCase().indexOf(serchParm.toUpperCase()) > - 1) {
+               li[i].style.display = "";
+           } else {
+               li[i].style.display = "none";
+           }
 
-//         }
-// }
+       }
+       });
 
-// mySearchFunction();
+       // li = ul.getElementsByTagName("li");
+       // for (i = 0; i< li.length; i++) {
+       //     a = li[i].getElementsByTagName("a")[0];
+       //     if (a.innerHTML.toUpperCase().indexOf(filter) > - 1) {
+       //         li[i].style.display = "";
+       //     } else {
+       //         li[i].style.display = "none";
+       //     }
+       //     console.log(i);
+
+       // }
+}
+
+mySearchFunction();
