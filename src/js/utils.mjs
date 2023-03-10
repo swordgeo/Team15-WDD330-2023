3066383fb2b24587ab6da5bffcef2552e740ea19
rodell1983 <ode21001@byui.edu>
2023-03-10 14:41:54 -0800

    sort function

--- src/js/utils.mjs
+++ src/js/utils.mjs
@@ -1,5 +1,3 @@
-
-
 //  wrapper for querySelector...returns matching element
 export function qs(selector, parent = document) {
   return parent.querySelector(selector);
@@ -9,7 +7,7 @@ export function qs(selector, parent = document) {
 
 // retrieve data from localstorage
 export function getLocalStorage(key) {
-  const item = localStorage.getItem(key)
+  const item = localStorage.getItem(key);
   //If there is no localStorage, return empty array
   return item ? JSON.parse(item) : [];
 }
@@ -36,19 +34,33 @@ export function setClick(selector, callback) {
 }
 
 //export function renderListWithTemplate(productCardTemplate, this.listElement, list);
-export function renderListWithTemplate(templateFn, parentElement, list, position = "afterbegin", clear = false){
+export function renderListWithTemplate(
+  templateFn,
+  parentElement,
+  list,
+  position = "afterbegin",
+  clear = false
+) {
   //if (clear) {
-    //parentElement.innerHTML() = ""
+  //parentElement.innerHTML() = ""
   //}
+  list = sortList(list,false,"price")
+  console.log(list);
 
   const htmlStrings = list.map(templateFn);
   parentElement.insertAdjacentHTML(position, htmlStrings.join(""));
-    // }
+  // }
 }
 
-export function renderWithTemplate(template, parentElement, position = "afterbegin", data, callback){
+export function renderWithTemplate(
+  template,
+  parentElement,
+  position = "afterbegin",
+  data,
+  callback
+) {
   parentElement.insertAdjacentHTML(position, template);
-  if(callback) {
+  if (callback) {
     callback(data);
   }
 }
@@ -60,12 +72,49 @@ export async function loadTemplate(path) {
 }
 
 export async function loadHeaderFooter(header, footer) {
-  const headerTemplate = await loadTemplate('../partials/header.html');
+  const headerTemplate = await loadTemplate("../partials/header.html");
   console.log(headerTemplate);
   renderWithTemplate(headerTemplate, header);
 
-  const footerTemplate = await loadTemplate('../partials/footer.html');
+  const footerTemplate = await loadTemplate("../partials/footer.html");
   renderWithTemplate(footerTemplate, footer);
+}
 
+export function sortList(list, ascending = true, value) {
+  switch (value) {
+    case "name":
+      if (ascending) {
+        return list.sort(function (a, b) {
+          if (a.Name < b.Name) {
+            return -1;
+          }
+          if (a.Name > b.Name) {
+            return 1;
+          }
+          return 0;
+        });
+      }else{
+        return list.sort(function (a, b) {
+          if (a.Name > b.Name) {
+            return -1;
+          }
+          if (a.Name < b.Name) {
+            return 1;
+          }
+          return 0;
+        });
+      }
 
+    case "price":
+      if (ascending) {
+        return list.sort(function (a, b) {
+          return parseFloat(a.FinalPrice) - parseFloat(b.FinalPrice);
+        });
+      }else{
+        return list.sort(function (a, b) {
+          return parseFloat(a.FinalPrice) + parseFloat(b.FinalPrice);
+        });
+      }
+      //return list
+  }
 }
